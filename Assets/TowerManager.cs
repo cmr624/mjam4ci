bca7c70f3af02f03e044e2630a421d0d02f0e0ad
Elliot Winch <18128280+elliot-winch@users.noreply.github.com>
2023-07-12 17:35:17 +0100

    Adding towers

---
+++ Assets/TowerManager.cs
@@ -0,0 +1,66 @@
+using Towers;
+using UnityEngine;
+
+public class TowerManager : MonoBehaviour
+{
+    public enum TowerGroupColour
+    {
+        Red,
+        Green,
+        Blue
+    }
+
+    public TowerGroup RedGroup { get; private set; }
+    public TowerGroup GreenGroup { get; private set; }
+    public TowerGroup BlueGroup { get; private set; }
+
+    private void Awake()
+    {
+        GreenGroup = new TowerGroup(name: "Green Group")
+        {
+            PlacementRules = new ITowerPlacementRule[]
+            {
+                new TPRMinimumDistance(minDistance: 5f),
+                //Add placement rules here
+            },
+            //Define the calculation for power level here
+            Calculator = new PLCMaxTowers(5),
+        };
+
+        RedGroup = new TowerGroup(name: "Red Group")
+        {
+            PlacementRules = new ITowerPlacementRule[]
+            {
+                new TPRMinimumDistance(minDistance: 10f),
+                //Add placement rules here
+            },
+            //Define the calculation for power level here
+            Calculator = new PLCMaxTowers(10),
+        };
+
+        BlueGroup = new TowerGroup(name: "Blue Group")
+        {
+            PlacementRules = new ITowerPlacementRule[]
+            {
+                new TPRMinimumDistance(minDistance: 20f),
+                //Add placement rules here
+            },
+            //Define the calculation for power level here
+            Calculator = new PLCMaxTowers(1),
+        };
+
+        //Add starting towers here:
+    }
+
+
+    public TowerGroup GetGroup(TowerGroupColour colour)
+    {
+        return colour switch
+        {
+            TowerGroupColour.Red => RedGroup,
+            TowerGroupColour.Green => GreenGroup,
+            TowerGroupColour.Blue => BlueGroup,
+            _ => null,
+        };
+    }
+}
