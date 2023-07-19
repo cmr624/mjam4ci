f9ecf687accf18418e0be0dfb0b86fd8d557085d
Carlos Michael <carlosmichaeldev@gmail.com>
2023-07-19 19:32:56 -0400

    added tower ui

--- Assets/Scripts/Towers/UI/GameObjectSwitcher.cs
+++ Assets/Scripts/Towers/UI/GameObjectSwitcher.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameObjectSwitcher : MonoBehaviour
 {
@@ -14,6 +15,9 @@ public class GameObjectSwitcher : MonoBehaviour
     [SerializeField]
     private Option[] m_gameObjects;
 
+    [SerializeField] private Toggle m_greenToggle;
+    [SerializeField] private Toggle m_redToggle;
+    [SerializeField] private Toggle m_blueToggle;
     public Topic<Option> Current { get; } = new Topic<Option>();
 
     private void Start()
@@ -50,6 +54,20 @@ public class GameObjectSwitcher : MonoBehaviour
         if (option.GameObject != null)
         {
             option.GameObject.SetActive(true);
+        }
+
+        switch (option.KeyCode)
+        {
+            // Alpha1 = green toggle, alpha 2 = red toggle, alpha 3 = blue toggle
+            case KeyCode.Alpha1:
+                m_greenToggle.isOn = true;
+                break;
+            case KeyCode.Alpha2:
+                m_redToggle.isOn = true;
+                break;
+            case KeyCode.Alpha3:
+                m_blueToggle.isOn = true;
+                break;
         }
 
         Current.Value = option;
