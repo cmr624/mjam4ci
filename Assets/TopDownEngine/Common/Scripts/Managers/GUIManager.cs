53f3c97a9c89d53252ce0a8c7486aa72d95036a6
Carlos Michael <carlosmichaeldev@gmail.com>
2023-07-19 22:10:40 -0400

    working on dashes

--- Assets/TopDownEngine/Common/Scripts/Managers/GUIManager.cs
+++ Assets/TopDownEngine/Common/Scripts/Managers/GUIManager.cs
@@ -64,6 +64,37 @@ namespace MoreMountains.TopDownEngine
 			Initialization();
 		}
 
+		
+		public MMProgressBar DashBar;
+		public MMProgressBar MissilesBar;
+		public MMProgressBar MeleeBar;
+
+		public void UpdateDashBar(float currentFuel, float minFuel, float maxFuel, string playerID)
+		{
+			if (DashBar == null)
+			{
+				return;
+			}
+			DashBar.UpdateBar(currentFuel, minFuel, maxFuel);
+		}
+
+		public void UpdateMissilesBar(float currentCooldownTimeLeft, float minTime, float maxTime)
+		{
+			if (MissilesBar == null)
+			{
+				return;
+			}
+			MissilesBar.UpdateBar(currentCooldownTimeLeft, minTime, maxTime);
+		}
+    
+		public void UpdateMeleeBar(float currentCooldownTimeLeft, float minTime, float maxTime)
+		{
+			if (MeleeBar == null)
+			{
+				return;
+			}
+			MeleeBar.UpdateBar(currentCooldownTimeLeft, minTime, maxTime);
+		}
 		protected virtual void Initialization()
 		{
 			if (_initialized)
